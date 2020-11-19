cef3cfea341e6c4cb35e5a395ee2410a63042850
avasalya <ashesh.vasalya@aist.go.jp>
2020-11-19 18:21:16 +0900

    replaced maskrcnn with yolact WIP: but not detecting onigiri

--- scripts/densefusion_ros.py
+++ scripts/densefusion_ros.py
@@ -76,8 +76,8 @@ class DenseFusion:
         self.pose_pub = rospy.Publisher('/onigiriPose', PoseArray, queue_size = 3)
         self.pose_sub = rospy.Subscriber('/onigiriPose', PoseArray, self.poseCallback, queue_size = 3)
 
-        # self.ts = message_filters.ApproximateTimeSynchronizer([rgb_sub, depth_sub], 9, 1)
-        self.ts = message_filters.TimeSynchronizer([rgb_sub, depth_sub], 9)
+        self.ts = message_filters.ApproximateTimeSynchronizer([rgb_sub, depth_sub], 9, 1)
+        # self.ts = message_filters.TimeSynchronizer([rgb_sub, depth_sub], 100)
         self.ts.registerCallback(self.callback)
 
         self.viz = np.zeros((480, 640, 3), np.uint8)
@@ -127,8 +127,9 @@ class DenseFusion:
             self.pose_estimator(rgb, depth)
 
             ''' publish to ros '''
-            # Publisher(self.model_pub, self.pose_pub, cam_mat,
-                    # self.viz, self.objs_pose, self.modelPts, self.cloudPts, 'camera_depth_optical_frame', method=None)
+            Publisher(self.model_pub, self.pose_pub, cam_mat,
+                    self.viz, self.objs_pose, self.modelPts, self.cloudPts,
+                    'camera_depth_optical_frame', method=None)
 
         except rospy.ROSException:
             print(f'{Fore.RED}ROS Interrupted')
@@ -320,140 +321,139 @@ class DenseFusion:
         obj_pose = []
         print(f'{Fore.GREEN}estimating pose..{Style.RESET_ALL}')
 
-        print('mask shape', mask.shape)
-        # pred = mask
-        # pred = pred *255
-        # pred = np.transpose(pred, (1, 2, 0)) # (CxHxW)->(HxWxC)
-        # if pred.shape[2] < 1:
-        #     print(f'{Fore.RED} no mask detected? {Style.RESET_ALL}', pred.shape)
+        if len(bbox) > 0:
+            pred = mask.cpu().numpy()
+            pred = pred *255
+            pred = np.transpose(pred, (1, 2, 0)) # (CxHxW)->(HxWxC)
 
-        # # convert img into tensor
-        # rgb_original = np.transpose(rgb, (2, 0, 1))
-        # norm = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+            # convert img into tensor
+            rgb_original = np.transpose(rgb, (2, 0, 1))
+            norm = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
-        # all_masks = []
-        # self.depth = depth.reshape(480, 640)
-        # mask_depth = ma.getmaskarray(ma.masked_not_equal(self.depth, 0))
-        # mask_label = ma.getmaskarray(ma.masked_equal(pred, np.array(255)))
+            self.depth = depth.reshape(480, 640)
+            mask_depth = ma.getmaskarray(ma.masked_not_equal(self.depth, 0))
+            mask_label = ma.getmaskarray(ma.masked_equal(pred, np.array(255)))
 
-        # # iterate through detected masks
-        # for b in range(len(bbox)):
+            # iterate through detected masks
+            for b in range(len(bbox)):
 
-        #     mask = mask_depth * mask_label[:,:,b]
-        #     rmin = int(bbox[b,0])
-        #     rmax = int(bbox[b,1])
-        #     cmin = int(bbox[b,2])
-        #     cmax = int(bbox[b,3])
+                mask = mask_depth * mask_label[:,:,b]
+                rmin = int(bbox[b,0])
+                rmax = int(bbox[b,1])
+                cmin = int(bbox[b,2])
+                cmax = int(bbox[b,3])
 
-        #     img = np.transpose(rgb_original, (0, 1, 2)) #CxHxW
-        #     choose = mask[rmin:rmax, cmin:cmax].flatten().nonzero()[0]
-        #     if len(choose) == 0:
-        #         cc = torch.LongTensor([0])
-        #         return(cc, cc, cc, cc, cc, cc)
+                img = np.transpose(rgb_original, (0, 1, 2)) #CxHxW
+                choose = mask[rmin:rmax, cmin:cmax].flatten().nonzero()[0]
+                if len(choose) == 0:
+                    cc = torch.LongTensor([0])
+                    return(cc, cc, cc, cc, cc, cc)
 
-        #     if len(choose) > num_points:
-        #         c_mask = np.zeros(len(choose), dtype=int)
-        #         c_mask[:num_points] = 1
-        #         np.random.shuffle(c_mask)
-        #         choose = choose[c_mask.nonzero()]
-        #     else:
-        #         choose = np.pad(choose, (0, num_points - len(choose)), 'wrap')
+                if len(choose) > num_points:
+                    c_mask = np.zeros(len(choose), dtype=int)
+                    c_mask[:num_points] = 1
+                    np.random.shuffle(c_mask)
+                    choose = choose[c_mask.nonzero()]
+                else:
+                    choose = np.pad(choose, (0, num_points - len(choose)), 'wrap')
 
-        #     depth_masked = self.depth[rmin:rmax, cmin:cmax].flatten()[choose][:, np.newaxis].astype(np.float32)
-        #     xmap_masked = self.xmap[rmin:rmax, cmin:cmax].flatten()[choose][:, np.newaxis].astype(np.float32)
-        #     ymap_masked = self.ymap[rmin:rmax, cmin:cmax].flatten()[choose][:, np.newaxis].astype(np.float32)
+                depth_masked = self.depth[rmin:rmax, cmin:cmax].flatten()[choose][:, np.newaxis].astype(np.float32)
+                xmap_masked = self.xmap[rmin:rmax, cmin:cmax].flatten()[choose][:, np.newaxis].astype(np.float32)
+                ymap_masked = self.ymap[rmin:rmax, cmin:cmax].flatten()[choose][:, np.newaxis].astype(np.float32)
 
-        #     choose = np.array([choose])
-        #     choose = torch.LongTensor(choose.astype(np.int32))
+                choose = np.array([choose])
+                choose = torch.LongTensor(choose.astype(np.int32))
 
-        #     pt2 = depth_masked
-        #     pt0 = (ymap_masked - cam_cx) * pt2 / cam_fx
-        #     pt1 = (xmap_masked - cam_cy) * pt2 / cam_fy
-        #     cloud = np.concatenate((pt0, pt1, pt2), axis=1)
-        #     points = torch.from_numpy(cloud.astype(np.float32))
+                pt2 = depth_masked
+                pt0 = (ymap_masked - cam_cx) * pt2 / cam_fx
+                pt1 = (xmap_masked - cam_cy) * pt2 / cam_fy
+                cloud = np.concatenate((pt0, pt1, pt2), axis=1)
+                points = torch.from_numpy(cloud.astype(np.float32))
 
-        #     img_masked = img[:, rmin:rmax, cmin:cmax ]
-        #     img_ = norm(torch.from_numpy(img_masked.astype(np.float32)))
-        #     idx = torch.LongTensor([self.object_index])
-        #     img_ = Variable(img_).cuda().unsqueeze(0)
-        #     points = Variable(points).cuda().unsqueeze(0)
-        #     choose = Variable(choose).cuda().unsqueeze(0)
-        #     idx = Variable(idx).cuda().unsqueeze(0)
+                img_masked = img[:, rmin:rmax, cmin:cmax ]
+                img_ = norm(torch.from_numpy(img_masked.astype(np.float32)))
+                idx = torch.LongTensor([self.object_index])
+                img_ = Variable(img_).cuda().unsqueeze(0)
+                points = Variable(points).cuda().unsqueeze(0)
+                choose = Variable(choose).cuda().unsqueeze(0)
+                idx = Variable(idx).cuda().unsqueeze(0)
 
-        #     pred_r, pred_t, pred_c, emb = self.estimator(img_, points, choose, idx)
-        #     pred_r = pred_r / torch.norm(pred_r, dim=2).view(1, num_points, 1)
-        #     pred_c = pred_c.view(bs, num_points)
-        #     how_max, which_max = torch.max(pred_c, 1) #1
-        #     pred_t = pred_t.view(bs * num_points, 1, 3)
+                pred_r, pred_t, pred_c, emb = self.estimator(img_, points, choose, idx)
+                pred_r = pred_r / torch.norm(pred_r, dim=2).view(1, num_points, 1)
+                pred_c = pred_c.view(bs, num_points)
+                how_max, which_max = torch.max(pred_c, 1) #1
+                pred_t = pred_t.view(bs * num_points, 1, 3)
 
-        #     my_r = pred_r[0][which_max[0]].view(-1).cpu().data.numpy()
-        #     my_t = (points.view(bs * num_points, 1, 3) + pred_t)[which_max[0]].view(-1).cpu().data.numpy()
-        #     my_pred = np.append(my_r, my_t)
+                my_r = pred_r[0][which_max[0]].view(-1).cpu().data.numpy()
+                my_t = (points.view(bs * num_points, 1, 3) + pred_t)[which_max[0]].view(-1).cpu().data.numpy()
+                my_pred = np.append(my_r, my_t)
 
-        #     ''' DF refiner NOTE: results are better without refiner '''
-        #     my_t, my_r = self.pose_refiner(1, my_t, my_r, points, emb, idx)
+                ''' DF refiner NOTE: results are better without refiner '''
+                my_t, my_r = self.pose_refiner(1, my_t, my_r, points, emb, idx)
 
-        #     ''' offset (mm) to align with obj-center '''
-        #     # get mean depth within a bbox as new object depth
-        #     meandepth = self.depth[rmin:rmax, cmin:cmax].astype(float)
-        #     # remove NAN and Zeros before taking depth mean
-        #     nonZero = meandepth[np.nonzero(meandepth)]
-        #     nonNaNDepth = np.nanmean(nonZero)
-        #     my_t[2] = nonNaNDepth
-        #     my_t[0] = my_t[0] + 20
-        #     my_t[1] = my_t[1] - 20
+                ''' offset (mm) to align with obj-center '''
+                # get mean depth within a bbox as new object depth
+                meandepth = self.depth[rmin:rmax, cmin:cmax].astype(float)
+                # remove NAN and Zeros before taking depth mean
+                nonZero = meandepth[np.nonzero(meandepth)]
+                nonNaNDepth = np.nanmean(nonZero)
+                my_t[2] = nonNaNDepth
+                my_t[0] = my_t[0] + 20
+                my_t[1] = my_t[1] - 20
 
-        #     ''' position mm2m '''
-        #     my_t = np.array(my_t*mm2m)
-        #     # print('Pos xyz:{0}'.format(my_t))
+                ''' position mm2m '''
+                my_t = np.array(my_t*mm2m)
+                # print('Pos xyz:{0}'.format(my_t))
 
-        #     ''' rotation '''
-        #     mat_r = quaternion_matrix(my_r)[:3, :3]
-        #     # mat_r = mat_r.transpose()
-        #     # print('estimated rotation is\n:{0}'.format(mat_r))
+                ''' rotation '''
+                mat_r = quaternion_matrix(my_r)[:3, :3]
+                # mat_r = mat_r.transpose()
+                # print('estimated rotation is\n:{0}'.format(mat_r))
 
-        #     ''' project depth point cloud '''
-        #     imgpts_cloud, jac = cv2.projectPoints(np.dot(points.cpu().numpy(), mat_r), mat_r, my_t, cam_mat, None)
-        #     viz = draw_pointCloud(viz, imgpts_cloud, [255, 0, 0]) # cloudPts
-        #     self.cloudPts = imgpts_cloud.reshape(num_points, 2)
+                ''' project depth point cloud '''
+                imgpts_cloud, jac = cv2.projectPoints(np.dot(points.cpu().numpy(), mat_r), mat_r, my_t, cam_mat, None)
+                viz = draw_pointCloud(viz, imgpts_cloud, [255, 0, 0]) # cloudPts
+                self.cloudPts = imgpts_cloud.reshape(num_points, 2)
 
-        #     ''' draw cmr 2D box '''
-        #     cv2.rectangle(viz, (cmax, cmin), (rmax, rmin), (255,0,0))
+                ''' draw cmr 2D box '''
+                cv2.rectangle(viz, (cmax, cmin), (rmax, rmin), (255,0,0))
 
-        #     ''' project the 3D bounding-box to 2D image plane '''
-        #     tvec, rvec, projPoints, center = draw_cube(self.modelPts, viz, mat_r, my_t, cam_mat)
+                ''' project the 3D bounding-box to 2D image plane '''
+                tvec, rvec, projPoints, center = draw_cube(self.modelPts, viz, mat_r, my_t, cam_mat)
 
-        #     ''' add PnP to DF's predicted pose'''
-        #     I = np.identity(4)
-        #     I[0:3, 0:3] = np.dot(mat_r.T, quaternion_matrix(rvec)[0:3, 0:3]) # mat_r
-        #     I[0:3, -1] =  my_t + np.asarray(tvec) *mm2m # my_t
-        #     rot = quaternion_from_matrix(I, True) #wxyz
+                ''' add PnP to DF's predicted pose'''
+                I = np.identity(4)
+                I[0:3, 0:3] = np.dot(mat_r.T, quaternion_matrix(rvec)[0:3, 0:3]) # mat_r
+                I[0:3, -1] =  my_t + np.asarray(tvec) *mm2m # my_t
+                rot = quaternion_from_matrix(I, True) #wxyz
 
-        #     ''' convert pose to ros-msg '''
-        #     my_t = I[0:3, -1]
-        #     my_t = my_t.reshape(1,3)
-        #     pose = {
-        #             'tx':my_t[0][0],
-        #             'ty':my_t[0][1],
-        #             'tz':my_t[0][2],
-        #             'qw':rot[0],
-        #             'qx':rot[1],
-        #             'qy':rot[2],
-        #             'qz':rot[3]}
+                ''' convert pose to ros-msg '''
+                my_t = I[0:3, -1]
+                my_t = my_t.reshape(1,3)
+                pose = {
+                        'tx':my_t[0][0],
+                        'ty':my_t[0][1],
+                        'tz':my_t[0][2],
+                        'qw':rot[0],
+                        'qx':rot[1],
+                        'qy':rot[2],
+                        'qz':rot[3]}
 
-        #     '''publish only if predicted pose is within the respective bbox'''
-        #     if  (min(cmax,rmax) < center[0] and center[0] < max(cmax,rmax)) and \
-        #         (min(cmin,rmin) < center[1] and center[1] < max(cmin,rmin)):
-        #         obj_pose.append(pose)
-        #     self.viz = viz
-        # else:
-        #     if len(bbox) < 1:
-        #         print(f'{Fore.RED}unable to detect pose..{Style.RESET_ALL}')
+                '''publish only if predicted pose is within the respective bbox'''
+                if  (min(cmax,rmax) < center[0] and center[0] < max(cmax,rmax)) and \
+                    (min(cmin,rmin) < center[1] and center[1] < max(cmin,rmin)):
+                    obj_pose.append(pose)
+                self.viz = viz
+            else:
+                if len(bbox) < 1:
+                    print(f'{Fore.RED}unable to detect pose..{Style.RESET_ALL}')
 
-        # self.objs_pose = obj_pose
+            self.objs_pose = obj_pose
 
-        t3 = time.time()
-        print(f'{Fore.YELLOW}DenseFusion inference time is:{Style.RESET_ALL}', t3 - t2)
+            t3 = time.time()
+            print(f'{Fore.YELLOW}DenseFusion inference time is:{Style.RESET_ALL}', t3 - t2)
+        else:
+            print(f'{Fore.RED}no mask or onigiri detected? {Style.RESET_ALL}', pred.shape)
 
 
 def main():
@@ -466,7 +466,7 @@ def main():
 
     try:
         rospy.spin()
-        rate = rospy.Rate(10)
+        rate = rospy.Rate(50)
         while not rospy.is_shutdown():
             rate.sleep()
     except KeyboardInterrupt:
